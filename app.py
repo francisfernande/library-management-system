88395d1799ae1003270e8f7df3e7e6742ce266f1
Francis Fernandes <fernandes.francis010@gmail.com>
2026-03-06 11:46:02 +0530

    done

--- app.py
+++ app.py
@@ -177,4 +177,4 @@ def logout():
 
 # ---------------- RUN APP ----------------
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000,debug=False)
